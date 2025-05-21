b89fa8c3182a9cdb5244b4fa5324602e1a61c0ed
Kaloyan776 <kpe3779@gmail.com>
2025-05-21 15:26:21 +0300

    upload

--- SelfServiceKaufland/SelfServiceKaufland/Program.cs
+++ SelfServiceKaufland/SelfServiceKaufland/Program.cs
@@ -39,77 +39,50 @@
                 }
             }
             Console.WriteLine("Вкарайте необходимите банкноти и/или монети:");
-            int Banknotes100 = 0;
-            int Banknotes50 = 0;
-            int Banknotes20 = 0;
-            int Banknotes10 = 0;
-            int Banknotes5 = 0;
-            int Coins2 = 0;
-            int Coins1 = 0;
-            int Coins050 = 0;
-            int Coins020 = 0;
-            int Coins010 = 0;
-            int Coins005 = 0;
-            int Coins002 = 0;
-            int Coins001 = 0;
 
             while (sum > 0)
             {
                 double banknoteOrCoin = double.Parse(Console.ReadLine());
-
                 switch (banknoteOrCoin)
                 {
                     case 100:
                         sum = sum - banknoteOrCoin;
-                        Banknotes100++;
                         break;
                     case 50:
                         sum = sum - banknoteOrCoin;
-                        Banknotes50++;
                         break;
                     case 20:
                         sum = sum - banknoteOrCoin;
-                        Banknotes20++;
                         break;
                     case 10:
                         sum = sum - banknoteOrCoin;
-                        Banknotes10++;
                         break;
                     case 5:
                         sum = sum - banknoteOrCoin;
-                        Banknotes5++;
                         break;
                     case 2:
                         sum = sum - banknoteOrCoin;
-                        Coins2++;
                         break;
                     case 1:
                         sum = sum - banknoteOrCoin;
-                        Coins1++;
                         break;
                     case 0.50:
                         sum = sum - banknoteOrCoin;
-                        Coins050++;
                         break;
                     case 0.20:
                         sum = sum - banknoteOrCoin;
-                        Coins020++;
                         break;
                     case 0.10:
                         sum = sum - banknoteOrCoin;
-                        Coins010++;
                         break;
                     case 0.05:
                         sum = sum - banknoteOrCoin;
-                        Coins005++;
                         break;
                     case 0.02:
                         sum = sum - banknoteOrCoin;
-                        Coins002++;
                         break;
                     case 0.01:
                         sum = sum - banknoteOrCoin;
-                        Coins001++;
                         break;
                     default:
                         Console.WriteLine("Това не е реална банкнота/монета и няма да бъде зачетена!");
